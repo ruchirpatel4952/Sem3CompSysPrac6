56edb9075b78c6b17aba43f4d6bccff00c1076ba
Ruchir <ruchirpatel2000@gmail.com>
2025-05-23 19:33:46 +0930

    first commit -- made some edits

--- VMTranslator/VMTranslator.cpp
+++ VMTranslator/VMTranslator.cpp
@@ -116,20 +116,68 @@ string VMTranslator::vm_if   (string label) {
 /*   Simple function support (enough for basic tests)          */
 /* ─────────────────────────────────────────────────────────── */
 
-string VMTranslator::vm_function(string name, int n_vars) {
+string VMTranslator::vm_function(string name, int nVars)
+{
     ostringstream out;
     out << '(' << name << ")\n";
-    for (int i = 0; i < n_vars; ++i)
+    for (int i = 0; i < nVars; ++i)
         out << "@0\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
     return out.str();
 }
 
-string VMTranslator::vm_call(string /*name*/, int /*n_args*/) {
-    return "// call-command not implemented in this minimal translator";
+
+string VMTranslator::vm_call(string name, int nArgs)
+{
+    ostringstream out;
+    int id = LABEL_COUNTER++;
+    string ret = "RET_" + to_string(id);
+
+    // push return address
+    out << '@' << ret << "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+
+    // push LCL, ARG, THIS, THAT
+    for (const char* seg : { "LCL", "ARG", "THIS", "THAT" }) {
+        out << '@' << seg << "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+    }
+
+    // ARG = SP - nArgs - 5
+    out << "@SP\nD=M\n"
+        << '@' << (nArgs + 5) << "\nD=D-A\n@ARG\nM=D\n";
+
+    // LCL = SP
+    out << "@SP\nD=M\n@LCL\nM=D\n";
+
+    // goto function
+    out << '@' << name << "\n0;JMP\n";
+
+    // (return address)
+    out << '(' << ret << ")\n";
+    return out.str();
 }
 
-string VMTranslator::vm_return() {
-    return "// return-command not implemented in this minimal translator";
+string VMTranslator::vm_return()
+{
+    ostringstream out;
+    out <<
+    // FRAME = LCL
+    "@LCL\nD=M\n@R13\nM=D\n"
+    // RET = *(FRAME-5)
+    "@5\nA=D-A\nD=M\n@R14\nM=D\n"
+    // *ARG = pop()
+    "@SP\nAM=M-1\nD=M\n@ARG\nA=M\nM=D\n"
+    // SP = ARG + 1
+    "@ARG\nD=M+1\n@SP\nM=D\n"
+    // THAT = *(FRAME-1)
+    "@R13\nAM=M-1\nD=M\n@THAT\nM=D\n"
+    // THIS = *(FRAME-2)
+    "@R13\nAM=M-1\nD=M\n@THIS\nM=D\n"
+    // ARG  = *(FRAME-3)
+    "@R13\nAM=M-1\nD=M\n@ARG\nM=D\n"
+    // LCL  = *(FRAME-4)
+    "@R13\nAM=M-1\nD=M\n@LCL\nM=D\n"
+    // goto RET
+    "@R14\nA=M\n0;JMP";
+    return out.str();
 }
 
 /* ─────────────────────────────────────────────────────────── */
